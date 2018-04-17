5d6f688cec34de20b860c814fe7a7ba1c27ac9f1
matsumur <matsumura.yuki.y2n@jp.nssol.nssmc.com>
2018-04-17 19:33:06 +0900

    Add label table

--- public/javascripts/controllers/RootController.js
+++ public/javascripts/controllers/RootController.js
@@ -29,7 +29,18 @@ $(() => {
     '{rootElement} rekognized': function(context, $el) {
       this.$find('#result_area').val(JSON.stringify(context.evArg));
       this.$find('#result_area')[0].parentNode.MaterialTextfield.checkDirty(); // これをやらないとテキストエリアのlabelが消えない
+      const tableBody = this.$find('#label-table>tbody');
+      for (const l of context.evArg.Labels) {
+        tableBody.append(createLabelTableRow(l.Name, l.Confidence));
+      }
     },
   };
   h5.core.controller('#container', RootController);
 });
+
+const createLabelTableRow = (label, confidence) => {
+  const tr = $('<tr>');
+  tr.append($('<td>').attr({class: 'mdl-data-table__cell--non-numeric'}).text(label));
+  tr.append($('<td>').text(confidence));
+  return tr;
+};
